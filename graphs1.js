1696346fd2d412d67a347a3814d5786a3b4bf9b7
Gingaling <nycgingsf@hotmail.com>
2022-07-12 07:38:48 -0700

    Committing my code to graphs1.js

--- graphs1.js
+++ graphs1.js
@@ -127,6 +127,23 @@ const edges: [
     ['o', 'n']
 ]
 
+function undirectedPath(edges, nodeA, nodeB) {
+    const graph = buildGraph(edges);
+    return hasPath(graph, nodeA, nodeB, new Set());
+}
+
+function hasPath(graph, src, dst, visited) {
+    if (src === dst) return true;
+    if (visited.has(src)) return false;
+    visited.add(src);
+    for (let neighbor of graph[src]) {
+        if (hasPath(graph, neighbor, dst)) === true) {
+            return true;
+        }
+    }
+    return false;
+}
+
 function buildGraph(edges) {
     const graph = {};
 
@@ -140,10 +157,6 @@ function buildGraph(edges) {
     return graph;
 }
 
-function undirectedPath(edges, nodeA, nodeB) {
-    const graph = buildGraph(edges);
-}
-
 const graph = {
     i:['j', 'k'],
     j:['i'],
@@ -152,4 +165,76 @@ const graph = {
     m:['k'],
     n:['o'],
     o:['n']
+}
+
+// Connected Components Count - Write a function, connectedComponentsCount, that takes in the adjacency list of an undirected graph. The function should return the number of connected components within the graph.
+
+// This graph has 3 components; if you draw it out, you will get 3 pieces to the graph
+
+const graph = {
+  3: [],
+  4: [6],
+  6: [4, 5, 7, 8],
+  8: [6],
+  7: [6],
+  5: [6],
+  1: [2],
+  2: [1]
+};
+
+function connectedComponentsCount(graph) {
+    const visited = new Set();
+    for (let node in graph) {
+        if (explore(graph, node, visited) === true) {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+function explore(graph, current, visited) {
+    if (visited.has(String(current))) return false;
+
+    visited.add(current);
+    for (let neighbor of graph[current]) {
+        explore(graph, neighbor);
+    }
+    return true;
+}
+
+// Largest Component - Write a function, largestComponent, that takes in the adjacency list of an undirected graph. The function should return the size of the largest connected component in the graph (time - O(e); space - O(n))
+
+// largest component = 4 (of 2)
+
+const graph = {
+  0: ['8', '1', '5'],
+  1: ['0'],
+  5: ['0', '8'],
+  8: ['0', '5'],
+  2: ['3', '4'],
+  3: ['2', '4'],
+  4: ['3', '2']
+}
+
+function largestComponent(graph) {
+    const visited = new Set();
+    let largest = 0;
+    for (let node in graph) {
+        let componentSize = explore(graph, node, visited);
+        if (componentSize >= largest){
+            largest = componentSize;
+        }
+    }
+    return largest;
+}
+
+function explore(graph, node, visited) {
+    if (visited.has(node)) return 0;
+
+    visited.add(node);
+    let size = 1;
+    for (let neighbor of graph[node]) {
+        size += explore(graph, neighbor, visited);
+    }
+    return size;
 }
